6dd2b90ec59cca3ccbc26bfbb535495609bdfe51
66Meg20 <66meg.dj2066@gmail.com>
2022-09-22 01:39:23 -0500

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -41,9 +41,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "eef40191214dcd4323d0468de9b004d0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-let apiKey = "eef40191214dcd4323d0468de9b004d0";
-let city = "amarillo";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("amarillo");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
